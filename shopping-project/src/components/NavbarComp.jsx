203a628dd4aa66b0f461e999b3a215e1d06b90cf
hj-Seong <hjseong1222@gmail.com>
2022-10-27 11:52:39 +0900

    1027 로그아웃

--- shopping-project/src/components/NavbarComp.jsx
+++ shopping-project/src/components/NavbarComp.jsx
@@ -5,19 +5,30 @@ import { Button } from "react-bootstrap";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShop } from "@fortawesome/free-solid-svg-icons";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import DataContext from "../context/DataContext";
 import { useEffect } from "react";
 
 function NavbarComp() {
+	//데이터
   const [login, setLogin] = useState(true);
 	const data = useContext(DataContext);
+	// 네비게이터
+	const navigate = useNavigate();
 
 	// 컴포넌트가 마운트 되자마자 로그인정보 확인
 	useEffect(()=>{
 		setLogin(data.state.user ? true : false )
-	})
+	}, [])
+	// Logout을 위한 이벤트 함수
+	const logOut = () => {
+		setLogin(false) // 컴포넌트를 바꾸주기 위해 수정
+		// userd의 값을 null로 바꿔줌
+		data.action.setUser(null);
+		// 다른곳에서 로그아웃을해도 항상 홈으로 돌아감
+		navigate("/");
+	}
 
   return (
     <>
@@ -38,7 +49,7 @@ function NavbarComp() {
                 <NavLink className="nav-link" to="/mypage">
                   {data.state.user.name}님의 MyPage
                 </NavLink>
-                <Button variant="outline-light">Logout</Button>{" "}
+                <Button variant="outline-light" onClick={ logOut }>Logout</Button>{" "}
               </Nav>
             ) : (
               <div>
