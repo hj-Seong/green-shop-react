4b8c746bf6e5c0c5b70749d84a31f1b86a0a5840
hjeong <hjseong1222@gmail.com>
2022-12-07 12:26:39 +0900

    1207 react-router-dom params 실습

--- study-react/src/page/Board.jsx
+++ study-react/src/page/Board.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const Board = () => {
+    const [boardlist, setBoardList] = useState([1,2,3,4,5])
     return ( 
         <div>
             <h1>Board 페이지입니다</h1>
-            <Link></Link>
+            <Link to='/board/1'>페이지 1로 이동합니다</Link>
+            <hr />
+            {/** 값이 여러개 일 경우 배열을 가져와 map으로 출력 */}
+            {
+                boardlist.map((boardid)=>(
+                    <Link to={`/board/${boardid}`}>{boardid}{" "}</Link>)
+                )
+            }
             <Outlet />
         </div>
      );
