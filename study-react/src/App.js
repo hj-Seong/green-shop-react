2fcdd2aaec79760116b01b388ecd0ecd7bcff8fa
hjeong <hjseong1222@gmail.com>
2022-12-07 12:34:41 +0900

    1207 react-router-dom params 실습

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -11,6 +11,7 @@ import TestHook from './components/TestHook';
 import APIComp from './components/APIComp';
 import TestAPI from './components/TestAPI';
 import RootIndex from './page/RootIndex';
+import TestRootIndex from './page/TestRootIndex';
 
 // App안에 원하는 내용을 바로 작성할 수 있지만
 // 각 내용을 컴포넌트로 만들어서 화면에 출력을 하고 있다
@@ -25,7 +26,7 @@ function App() {
   // return 안에 원하는 컴포넌트를 넣어서 확인하면서 공부하시면됩니다
   return (
     <div className="App">
-      <RootIndex />
+      <TestRootIndex />
     </div>
   );
 }
