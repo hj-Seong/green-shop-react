07bb816fc297e9e1aef4b5392ed5568ab69050a0
hjeong <hjseong1222@gmail.com>
2022-12-09 12:37:18 +0900

    1209 react-redux 리덕스 실습 확인

--- study-react/src/Redux/modules/book.js
+++ study-react/src/Redux/modules/book.js
@@ -9,11 +9,19 @@ const initalState = {
 /** setTitle 을 작성하여 payload로 값이 들어왔을때
  * 그 값으로 title을 수정하는 case 작성
  */
-
+function book (state = initalState, action) {
+    switch (action.type) {
+        case "setTitle" :
+            return { ...state, title : action.payload}
+        default :
+            return state;
+    }
+}
 
 // 액션함수 만들기
-
+export const setTitle = title => ({type : "setTitle", payload : title})
 
 // 리듀서 내보내기
+export default book;
 
 // >> 리듀서 modules/index.js에 추가하기
--- study-react/src/Redux/modules/index.js
+++ study-react/src/Redux/modules/index.js
@@ -7,10 +7,11 @@ import { combineReducers } from "redux";
 
 //작성한 리덕스 모듈 들고오기
 import number from "./number";
+import book from "./book";
 
 // combinReducer를 통해서 
 // 작성한 js 파일을 들고와서 객체 형태로 묶어준다
-const rootReducer = combineReducers({number});
+const rootReducer = combineReducers({number,book});
 
 // export default를 통해서 
 // 이름을 그대로 사용할수 있게 내보내준다
