3e152d0e905d0cf0ec860242e99c7b4335987064
hjeong <hjseong1222@gmail.com>
2022-12-13 12:28:12 +0900

    1213 리덕스기초 복습2

--- study-react/src/Redux/modules/book.js
+++ study-react/src/Redux/modules/book.js
@@ -4,7 +4,6 @@ const initalState = {
     pay : 5000,
 }
 
-
 // 리듀서 작성
 /** setTitle 을 작성하여 payload로 값이 들어왔을때
  * 그 값으로 title을 수정하는 case 작성
@@ -17,6 +16,8 @@ function book (state = initalState, action) {
     switch (action.type) {
         case "setTitle" :
             return { ...state, title : action.payload}
+        case "setPay":
+            return { ...state, pay : action.payload }
         default :
             return state;
     }
@@ -24,6 +25,7 @@ function book (state = initalState, action) {
 
 // 액션함수 만들기
 export const setTitle = title => ({type : "setTitle", payload : title})
+export const setPay = pay => ({type :"setPay", payload : pay})
 
 // 리듀서 내보내기
 export default book;
