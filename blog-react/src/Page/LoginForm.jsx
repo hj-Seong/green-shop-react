e202bb126c5733b8748703111f16575f73613731
hjeong <hjseong1222@gmail.com>
2022-12-16 11:48:06 +0900

    1216 blog-firbase-auth-email

--- blog-react/src/Page/LoginForm.jsx
+++ blog-react/src/Page/LoginForm.jsx
@@ -2,14 +2,48 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 const LoginForm = () => {
+  // 페이지를 이동하기위한 navigate();
+  const navigate = useNavigate();
+
+  // 이메일과 비밀번호를 가져올 state
   const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  // 이메일로 회원가입하기위한 함수
+  const emailCreate = () => {
+    // getAuth는 파이어베이스앱에서 인증 부분을 받아오는 함수
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 회원가입성공
+        const user = userCredential.user;
+        console.log(user)
+        navigate('/');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode)
+        if ( errorCode == "auth/email-already-in-use") {
+          alert("이미 사용하고 있는 이메일입니다")
+        }
+        else if ( errorCode=="auth/weak-password") {
+          alert("비밀번호를 6자리 이상으로 작성하세요");
+        }
+    });
+    
+  }
+
   return (
     <div>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>이메일</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={(e)=>{setEmail(e.target.value)}}/>
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -17,13 +51,13 @@ const LoginForm = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>비밀번호</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}} />
         </Form.Group>
         <Button variant="primary" type="submit">
           로그인
         </Button>
       </Form>
-      <Button >위 이메일과 비밀번호로 회원가입</Button>
+      <Button onClick={emailCreate}>위 이메일과 비밀번호로 회원가입</Button>
       <Button >구글로 로그인</Button>
     </div>
   );
