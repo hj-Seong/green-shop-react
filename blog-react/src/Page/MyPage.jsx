c13141f3f8d0252144ffbaca89132bb11daecbfa
hjeong <hjseong1222@gmail.com>
2022-12-28 16:31:21 +0900

    1228 blog-toastUI 추가수정

--- blog-react/src/Page/MyPage.jsx
+++ blog-react/src/Page/MyPage.jsx
@@ -1,23 +1,27 @@
-import {useSelector} from 'react-redux'
+import { Col, Container, Row } from 'react-bootstrap';
+import { useSelector} from 'react-redux'
 const MyPage = () => {
     const user = useSelector((state)=>state.currentUser)
     const userInfo = useSelector((state)=>state.userInfoList)
                     .find((info)=>(info.userEmail == user.email))
     return ( 
-        <div>
-            <h3>유저 페이지</h3>
-            <p>유저 이메일 : {userInfo.userEmail}</p>
-            
-            <h5>좋아요 리스트</h5>
-            <ul>
-                {
-                    userInfo.like.map((l)=>(
-                        <li key={l.boardId}>{l.title}</li>
-                    ))
-                }
-                
-            </ul>
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <p>유저 이메일 : {userInfo.userEmail}</p>
+                </Col>
+                <Col style={{border:"solid 1px grey", textAlign:"center"}}>
+                    <h5>좋아요 리스트</h5>
+                    <ul>
+                        {
+                            userInfo.like.map((l)=>(
+                                <li key={l.boardId}>{l.title}</li>
+                            ))
+                        }
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
