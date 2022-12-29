e8b4b19f64236eff6e912120bb288932b0a41c81
hjeong <hjseong1222@gmail.com>
2022-12-29 18:13:18 +0900

    blog-구글인증 이미지업데이트

--- blog-react/src/Page/MyPage.jsx
+++ blog-react/src/Page/MyPage.jsx
@@ -1,14 +1,27 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import { useSelector} from 'react-redux'
+import { getAuth } from "firebase/auth";
+import ProfileUpdateModal from '../components/ProfileUpdateModal';
+import { useEffect } from 'react';
+import { useState } from 'react';
 const MyPage = () => {
+    const auth = getAuth();
     const user = useSelector((state)=>state.currentUser)
     const userInfo = useSelector((state)=>state.userInfoList)
                     .find((info)=>(info.userEmail == user.email))
+    const [update, setUpdate] = useState(false);
+    useEffect(()=>{
+        console.log("확인")
+    }, [update])
     return ( 
         <Container>
             <Row>
                 <Col>
                     <p>유저 이메일 : {userInfo.userEmail}</p>
+                    {
+                        auth.currentUser && <img src={auth.currentUser.photoURL} alt=""  style={{width:"300px"}}/>
+                    }
+                    <ProfileUpdateModal  setUpdate={setUpdate}/>
                 </Col>
                 <Col style={{border:"solid 1px grey", textAlign:"center"}}>
                     <h5>좋아요 리스트</h5>
